6ac6bb559b2febcae16d707630d64779adbc5272
fikriadriansa21 <fikriadriansa21@gmail.com>
2020-06-27 08:44:45 +0700

    set dimen file, layouting, welcome page and success feedback registration, setup gradle compatibility with glide,retrofit, google-auth for sign in

---
+++ app/src/main/java/com/example/myfootballmatch/success/SuccessRegistrationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myfootballmatch.success
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfootballmatch.R
+
+class SuccessRegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_registration)
+    }
+}
